1058ec443b6d75a3f03d9a5bc2b585509559d45e
khatrironit <ronitkhatri1999@gmail.com>
2020-09-17 03:24:03 +0530

    connected to database and oxford api

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,36 @@
 const express = require('express')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
 
 const vocabRoute = require('./routes/vocab')
+const addWordRoute = require('./routes/addWord')
 
+//connect to Mongo DB
+mongoose.connect('mongodb+srv://admin:ronitkhatri@vocab.mavsc.mongodb.net/Dictionary?retryWrites=true&w=majority',
+{ useUnifiedTopology: true,useNewUrlParser: true })
 //logs the incoming api requests.
 app.use(logger('dev'))
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+//handling cors error
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization')
+    
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods","PUT,POST,GET,PATCH,DELETE")
+        return res.status(200).json({})
+    }
+    next()
+})
 //Route to handle api requests.
 app.use('/',vocabRoute)
-
+app.use('/add',addWordRoute)
 //middleware to handle api requests to invalid routes.
 app.use((req,res,next)=>{
     const error = new Error('Page Not Found!!')
